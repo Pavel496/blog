86eb439f32b429ac24a654554c3496251f5f0d8f
Pavel496 <butusovpn@mail.ru>
2018-03-04 11:20:18 +0300

    little more

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -13,57 +13,38 @@
 @endsection
 
 @section('content')
-
-<div class="row">
-
-  <form>
-
-  <div class="col-md-8">
-
-    <div class="box box-primary">
-
-      {{-- <div class="box-header">
-
-        <h3 class="box-title">Create post</h3>
-
-      </div> --}}
-
-        <div class="box-body">
-
-          <div class="form-group">
-            <label>Title</label>
-            <input name="title" class="form-control" placeholder= "Enter title of post">
-          </div>
-
-          <div class="form-group">
-            <label>Body</label>
-            <textarea rows="10" name="body" class="form-control" placeholder= "Enter body of post"></textarea>
-          </div>
-
-        </div>
-
-
-
-    </div>
-
-  </div>
-
-  <div class="col-md-4">
-    <div class="box box-primary">
-      {{-- <div class="box-header">
-        <h3 class="box-title"></h3>
-      </div> --}}
-
-      <div class="box-body">
-        <div class="form-group">
-          <label>Excerpt</label>
-          <textarea name="excerpt" class="form-control" placeholder= "Enter excerpt of post"></textarea>
+  <div class="row">
+    <form>
+      <div class="col-md-8">
+        <div class="box box-primary">
+          {{-- <div class="box-header">
+            <h3 class="box-title">Create post</h3>
+          </div> --}}
+            <div class="box-body">
+              <div class="form-group">
+                <label>Title</label>
+                <input name="title" class="form-control" placeholder= "Enter title of post">
+              </div>
+              <div class="form-group">
+                <label>Body</label>
+                <textarea rows="10" name="body" class="form-control" placeholder= "Enter body of post"></textarea>
+              </div>
+            </div>
         </div>
       </div>
-
-    </div>
+      <div class="col-md-4">
+        <div class="box box-primary">
+          {{-- <div class="box-header">
+            <h3 class="box-title"></h3>
+          </div> --}}
+          <div class="box-body">
+            <div class="form-group">
+              <label>Excerpt</label>
+              <textarea name="excerpt" class="form-control" placeholder= "Enter excerpt of post"></textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
-</form>
-</div>
-
 @endsection
