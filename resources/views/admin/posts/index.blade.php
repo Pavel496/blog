c6ed25b70655bf942efe03efca7a905e84881e8c
Pavel496 <butusovpn@mail.ru>
2018-03-15 15:53:23 +0300

    Revert "Revert "Revert "39, 40, 41"""
    This reverts commit 70889d29a83a7f75b1749a70dd78c17d35e92be5.

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -42,16 +42,8 @@
                             class="btn btn-xs btn-default"
                             target="_blank">
                             <i class="fa fa-eye"></i></span></a>
-                          <a href="{{ route('admin.posts.edit', $post) }}"
-                            class="btn btn-xs btn-info">
-                            <i class="fa fa-pencil"></i></span></a>
-                          <form method="POST"
-                            action="{{ route('admin.posts.destroy', $post) }}"
-                            style="display: inline">
-                            {{ csrf_field() }} {{ method_field('DELETE') }}
-                            <button class="btn btn-xs btn-danger"><i class="fa fa-times"></i></button>
-                          </form>
-
+                          <a href="{{ route('admin.posts.edit', $post) }}" class="btn btn-xs btn-info"><i class="fa fa-pencil"></i></span></a>
+                          <a href="#" class="btn btn-xs btn-danger"><i class="fa fa-times"></i></span></a>
                         </td>
                       </tr>
                     @endforeach
